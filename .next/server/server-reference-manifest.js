312c1e9fef72a584385935df99ac991a8f915d47
Mayur Ingle <imayur1588@outlook.com>
2026-04-28 08:38:47 +0530

    feat: add Volume 1 mission log content and generate initial PDF assets

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"YRTkdyPsEVtEKHlOUdwH0JkyLTW1GREUmd9RT/Q8iBc=\"\n}"
--- src/app/volume-1/mission-log/page.tsx
+++ src/app/volume-1/mission-log/page.tsx
@@ -1,11 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-import { MDXRemote } from 'next-mdx-remote/rsc';
-import { mdxComponents } from '@/components/mdx';
+import fs from "fs";
+import path from "path";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import { mdxComponents } from "../../../components/mdx";
 
 export default function MissionLogPage() {
-  const filePath = path.join(process.cwd(), 'src/content/volume-1/mission-log.mdx');
-  const source = fs.readFileSync(filePath, 'utf8');
+  const filePath = path.join(
+    process.cwd(),
+    "src/content/volume-1/mission-log.mdx",
+  );
+  const source = fs.readFileSync(filePath, "utf8");
 
   return (
     <main className="max-w-4xl mx-auto p-8 bg-white print:p-0 print:max-w-none">
